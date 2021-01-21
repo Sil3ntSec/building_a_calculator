4b1a5778a0543fdffd5cf3b2fdff4b570baf09a4
Luke McManamon <48098579+Sil3ntSec@users.noreply.github.com>
2021-01-21 17:33:02 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import re
+
+print("Our Magical Calculator")
+print("Type 'quit' to exit\n")
+
+previous = 0
+run = True
+
+def performMath():
+    global run
+    global previous
+    equation = ""
+
+    # If there has been a previous calculation, use that result as the prompt
+    if previous == 0:
+        equation = input("Enter equation:")
+    else:
+        equation = input(str(previous))
+
+    # if user quits ->
+    if equation == 'quit':
+        print("Goodbye, human")
+        run = False
+    else:
+        equation = re.sub('[a-zA-Z,.:()" "]', '', equation)
+        if previous == 0:
+            previous = eval(equation)
+        else:
+            previous = eval(str(previous) + equation)
+
+while run:
+    performMath()
